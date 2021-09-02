75fa18644cb4f8fbbed6fac0cf84a4830781d9d0
fenghuolun7 <825693784@qq.com>
2021-09-02 08:59:25 -0400

    test

--- listings/chapter_2/2.1_explian.cpp
+++ listings/chapter_2/2.1_explian.cpp
@@ -13,7 +13,7 @@ struct func
 
     void operator()()
     {
-        for(unsigned j=0;j<100;++j)
+        for(unsigned j=0;j<1000000;++j)
         {
             do_something(i);
         }
@@ -25,7 +25,9 @@ void oops()
 {
     int some_local_state=0;
     func my_func(some_local_state);
-    std::thread my_thread(my_func);
+
+    std::thread my_thread{my_func};
+
     my_thread.detach();
 }
 
